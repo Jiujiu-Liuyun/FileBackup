8a0b63d04f859a2d811deb5c4b09eaea208b0e48
zhangyun <2604493210@qq.com>
2022-07-22 00:11:03 +0800

    git初始化

---
+++ src/main/java/com/zhangyun/tools/filebackup/FileBackupApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyun.tools.filebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBackupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangyun/tools/filebackup/FileBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangyun.tools.filebackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
