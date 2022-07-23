321d2ad83b61e4bdc4887f7a2f1d04d861243e0c
zhangyun <2604493210@qq.com>
2022-07-23 21:50:44 +0800

    日志输出配置 1.0

--- src/main/java/com/zhangyun/tools/filebackup/FileBackupApplication.java
+++ src/main/java/com/zhangyun/tools/filebackup/FileBackupApplication.java
@@ -1,15 +1,20 @@
 package com.zhangyun.tools.filebackup;
 
 import com.zhangyun.tools.filebackup.filevisitor.CounterFileVisitor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FileBackupApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileBackupApplication.class, args);
+        log.info("服务启动成功！");
+        log.warn("this is warn message!");
+        log.error("this is warn message!");
     }
 
 }
